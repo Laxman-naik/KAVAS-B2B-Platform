99a00f9db427d856cf73998eadc11d0a8300052e
Laxman Naik Bhukya <laxmanbhukya712@gmail.com>
2026-04-02 17:18:01 +0530

    url changed

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,11 +1,11 @@
 import api from "@/lib/axios";
 
-export const registerUserAPI = (data) => api.post("api/auth/register", data);
+export const registerUserAPI = (data) => api.post("/api/auth/register", data);
 
-export const loginUser = (data) => api.post("api/auth/login", data);
+export const loginUser = (data) => api.post("/api/auth/login", data);
 
-export const refreshToken = () => api.post("api/auth/refresh");
+export const refreshToken = () => api.post("/api/auth/refresh");
 
-export const logoutUser = () => api.post("api/auth/logout");
+export const logoutUser = () => api.post("/api/auth/logout");
 
-export const getMe = () => api.get("api/auth/me");
+export const getMe = () => api.get("/api/auth/me");
