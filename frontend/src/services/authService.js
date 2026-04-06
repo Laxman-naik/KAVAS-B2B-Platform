07a453578a260fe7f4d10b5ee64c3217740eccba
Laxman Naik Bhukya <laxmanbhukya712@gmail.com>
2026-04-06 09:16:03 +0530

    axios change

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,12 +1,13 @@
 import api from "@/lib/axios";
+import axios from "axios";
 
 export const registerUserAPI = (data) => api.post("/api/auth/register", data);
 
-export const loginUser = (data) => api.post("/api/auth/login", data);
+// export const loginUser = (data) => api.post("/api/auth/login", data);
+export const loginUser = (data) => axios.post("https://kavas-b2b-platform-3.onrender.com/api/auth/login", data)
 
 export const refreshToken = () => api.post("/api/auth/refresh");
 
-// export const logoutUser = () => api.post("/api/auth/logout");
-axios.post("https://kavas-b2b-platform-3.onrender.com/api/auth/login", data)
+export const logoutUser = () => api.post("/api/auth/logout");
 
 export const getMe = () => api.get("/api/auth/me");
