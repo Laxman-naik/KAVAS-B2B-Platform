9abb3aacbc368d821181e21dc9737e0fd45e3deb
Laxman Naik Bhukya <laxmanbhukya712@gmail.com>
2026-04-09 13:01:36 +0530

    axios change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const AUTH_BASE_URL = "https://kavas-b2b-platform-2.onrender.com";
-const PRODUCT_BASE_URL = "https://kavas-b2b-platform-2.onrender.com";
+const AUTH_BASE_URL = "https://kavas-b2b-platform-3.onrender.com";
+const PRODUCT_BASE_URL = "https://kavas-b2b-platform-4.onrender.com";
 
 const authapi = axios.create({ baseURL: AUTH_BASE_URL, withCredentials: true,});
 
