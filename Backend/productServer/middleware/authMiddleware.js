bb37066393e6bdb0f4a8a254c30ce3495847ba1c
Laxman Naik Bhukya <laxmanbhukya712@gmail.com>
2026-04-19 11:54:22 +0530

    cart apis updating

--- Backend/productServer/middleware/authMiddleware.js
+++ Backend/productServer/middleware/authMiddleware.js
@@ -49,15 +49,39 @@
 
 // module.exports = authMiddleware;
 
+// const jwt = require("jsonwebtoken");
+
+// const authMiddleware = (req, res, next) => {
+//   try {
+//     const token = req.cookies?.accessToken;
+
+//     if (!token) {
+//       return res.status(401).json({ message: "No token" });
+//     }
+
+//     const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+
+//     req.user = decoded;
+//     next();
+//   } catch (err) {
+//     console.log("AUTH ERROR:", err.message);
+//     return res.status(401).json({ message: "Invalid token" });
+//   }
+// };
+
+// module.exports = authMiddleware;
+
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   try {
-    const token = req.cookies?.accessToken;
+    const authHeader = req.headers.authorization;
 
-    if (!token) {
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ message: "No token" });
     }
+
+    const token = authHeader.split(" ")[1];
 
     const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
 
