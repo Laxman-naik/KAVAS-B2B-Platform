67b6842e12fdbb4596fe622d33ae456976deab69
Laxman Naik Bhukya <laxmanbhukya712@gmail.com>
2026-05-07 15:22:04 +0530

    changes in vendor registation

--- Backend/AuthServer/src/controllers/adminController.js
+++ Backend/AuthServer/src/controllers/adminController.js
@@ -293,7 +293,7 @@ const approveVendor = async (req, res) => {
   } catch (err) {
     console.error(err);
     return res.status(500).json({
-      message: "Server error",
+      message: err,
       error: err.message,
     });
   }
