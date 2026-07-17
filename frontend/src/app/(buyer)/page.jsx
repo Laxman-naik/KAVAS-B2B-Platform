6ffd6324b3ed5f123cf1e37b385fb57c2ef1ebf1
Srikanth771 <srikanthofficial1910@gmail.com>
2026-07-17 21:54:50 +0530

    conflicts

--- frontend/src/app/(buyer)/page.jsx
+++ frontend/src/app/(buyer)/page.jsx
@@ -1,29 +1,3 @@
-// import Home from "@/components/ui/common/Home";
-// import TrendingProducts from "@/components/ui/common/TrendingProducts";
-// import NewArrivals from "@/components/ui/common/NewArrivals";
-// import FeaturedSuppliers from "@/components/ui/common/FeaturedSuppliers";
-// import FlashDeals from "@/components/ui/common/FlashDeals";
-// import TrustedSlide from "@/components/ui/common/TrustedSlide";
-// import AllProducts from "@/components/ui/common/AllProducts";
-// import { Categories } from "@/components/ui/common/Categories";
-
-// export default function BuyerHome() {
-//   return (
-
-//     <>
-    
-//       <Home />
-//       <NewArrivals />
-//       <FlashDeals />
-//       <TrendingProducts />
-//       <TrustedSlide />
-//       <AllProducts />
-//       <FeaturedSuppliers />
-      
-//     </>
-//   );
-// }
-
 
 import Home from "@/components/ui/common/Home";
 import TrendingProducts from "@/components/ui/common/TrendingProducts";
